2527aa60ab2281532e9b3616841ac5aafab808b6
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-07-01 05:33:00 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,38 +1,17 @@
 "use strict";
 
+let count = 0;
+const value = document.getElementById("value");
 const buttons = document.querySelectorAll(".btn");
-const increaseBtn = document.getElementById("increase");
-const decreaseBtn = document.getElementById("decrease");
-const resetBtn = document.getElementById("reset");
-let counter = document.getElementById("counter");
-let inicialCounter = counter.getAttribute("data-counter");
-
-console.log(inicialCounter);
-counter.textContent = inicialCounter;
+value.textContent = count;
+console.log(buttons);
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const btnId = button.getAttribute("id");
-    if (btnId === "increase") increaseCounter();
-    if (btnId === "decrease") decreaseCounter();
-    if (btnId === "reset") resetCounter();
+  button.addEventListener("click", (e) => {
+    const btnClassList = e.target.classList;
+    if (btnClassList.contains("increase")) count++;
+    if (btnClassList.contains("decrease")) count--;
+    if (btnClassList.contains("reset")) count = 0;
+    value.textContent = count;
   });
 });
-
-function changeCounterColor() {
-  if (counter < 0) counter.style.color = "#c40d0d";
-  if (counter > 0) counter.style.color = "#03a83a";
-}
-function increaseCounter() {
-  inicialCounter++;
-  counter.textContent = inicialCounter;
-  changeCounterColor();
-}
-function decreaseCounter() {
-  counter.textContent -= 1;
-  changeCounterColor();
-}
-function resetCounter() {
-  inicialCounter = 0;
-  changeCounterColor();
-}
