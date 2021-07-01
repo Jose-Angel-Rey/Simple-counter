36149c92786538e473050abdc0b81e91214ef3b0
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-07-01 05:47:38 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -3,15 +3,20 @@
 let count = 0;
 const value = document.getElementById("value");
 const buttons = document.querySelectorAll(".btn");
+
 value.textContent = count;
-console.log(buttons);
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const btnClassList = e.target.classList;
+    // Change counter value
     if (btnClassList.contains("increase")) count++;
     if (btnClassList.contains("decrease")) count--;
     if (btnClassList.contains("reset")) count = 0;
+    // Change counter value color
+    if (count > 0) value.style.color = "#106310";
+    if (count < 0) value.style.color = "#c90303";
+    if (count === 0) value.style.color = "#888";
     value.textContent = count;
   });
 });
